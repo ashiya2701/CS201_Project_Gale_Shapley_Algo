3e090bab2414132ab90115865bca2a65b716f566
Ashiya Kandhway <ashiyakandhway27july@gmail.com>
2021-11-21 18:30:33 +0530

    Update project.c

--- project.c
+++ project.c
@@ -38,9 +38,9 @@ void gale_shapley_algorithm(int n)
         last_uni[i]=0;
     }
     //calculating Pref[][]
-    for (j=0;j<=n;++j) 
+    for (j=1;j<=n;++j) 
     {
-		for (i=0 ;i<=n;++i) 
+		for (i=1 ;i<=n;++i) 
         {
 			Pref[Uni_Pref[j][i]][j]=i;
 		}
